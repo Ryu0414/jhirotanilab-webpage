93c756edf8d38cb9c2f37040903e96e8acb073f9
yamasakiryuto <y.ryuto20000414@gmail.com>
2024-10-04 08:56:25 +0900

    画像テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-// import Image from "next/image";
+import Image from "next/image";
+import LogoImage from "./_images/Unknown.png";
 // import nextConfig from "../../next.config.mjs";
 // const BASE_PATH = nextConfig.basePath || "";
 
 export default function Home() {
-  return <div>ホームページ開発</div>;
+  return (
+    <div className="flex flex-col items-center gap-10">
+      <div className="font-bold text-3xl">Welcome to J.HIROTANI Webpage!!!</div>
+      <Image src={LogoImage} alt="logo" className="w-1/2" />
+    </div>
+  );
 }
