cf1aa573b839d0b79bf20c07f2341eb1f3c1e2e9
yamasakiryuto <y.ryuto20000414@gmail.com>
2024-10-11 19:19:35 +0900

    研究紹介修正

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -15,21 +15,21 @@ export default function Research() {
             <ResearchLi
               image={FDTRImage}
               title="Nanoscale and microscale thermal transport"
-              details="details details details details details details details details details details details details details details details details details details details details details "
+              details="When we pursue the highest performance of nano-electrical devices, it becomes crucial to manage the heat generation and dissipation, where thermal transport might be dominated by the thermal boundary resistance at the interfaces. We have explored nanoscale and microscale thermal transport by Raman thermal measurement and thermoreflectance technique with combinations of first-principle/molecular-dymanics calculations."
             />
           </div>
           <div id="flexible">
             <ResearchLi
               image={FlexibleImage}
               title="Flexible electronics"
-              details="details details details details details details details details details details details details details details details details details details details details details "
+              details="In recent years, flexible electronics have attracted considerable attention due to the wide range of potential applications, from flexible displays to wearable healthcare devices. For example. carbon nanotube thin-film transistors (CNT TFTs) are considered to be promising building blocks for flexible electronics because of their remarkable electrical and mechanical properties. In my research group, flexible device applications based on nanomaterials have been explored for future IoT and healthcare devices."
             />
           </div>
           <div id="device">
             <ResearchLi
               image={DeviceImage}
               title="Electronic and thermal devices for tissue engineering"
-              details="details details details details details details details details details details details details details details details details details details details details details "
+              details="As the field of tissue engineering advances, new tools for monitoring and evaluating of engineered tissues along with new biomaterials to direct tissue growth are needed. We use carbon nanotubes as an important tissue engineering material for improved tracking of cells, sensing of microenvironments, delivering of transfection agents, and scaffolding for incorporating with the host’s body. In addition, monitoring and alteringintra and intercellular processes would be useful for design of better engineered tissues. Since 2020, we have just started to make electronic and thermal device for creating and monitoring engineered tissue."
             />
           </div>
           <div>
