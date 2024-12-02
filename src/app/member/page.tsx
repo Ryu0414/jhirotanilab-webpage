0e4244a3321fc61f89bb1e890dda1924313d6eec
yamasakiryuto <y.ryuto20000414@gmail.com>
2024-12-02 15:46:34 +0900

    Memberページモバイル対応

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -3,30 +3,36 @@ import Image from "next/image";
 export default function Member() {
   return (
     <div className="pt-20 flex flex-col items-center">
-      <div className="text-4xl font-bold self-center pt-20">Member</div>
-      <div className="w-2/3 flex flex-col gap-6 mt-20 items-center">
+      <div className="text-4xl font-bold self-center pt-10 lg:pt-20">
+        Member
+      </div>
+      <div className="w-5/6 lg:w-2/3 flex flex-col gap-6 lg:gap-6 mt-10 lg:mt-20 items-center">
         <div className="text-3xl font-bold">STAFF</div>
-        <div className="flex flex-row w-full items-center">
+        <div className="flex flex-col lg:flex-row w-full items-center">
           <Image
-            src={"/_images/hirotani-sensei.jpeg"}
+            src={"/_images/hirotani-sensei.webp"}
             width={100}
             height={100}
             alt="Hirotani-sensei"
-            className="w-1/2 p-10"
+            className="w-full md:w-2/3 lg:w-1/2 p-10"
           />
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
-              <p className="text-3xl font-bold">Jun Hirotani, Ph.D.</p>
-              <p className="text-xl">Email: hirotani.jun.7v@kyoto-u.ac.jp</p>
+              <p className="text-2xl lg:text-3xl font-bold">
+                Jun Hirotani, Ph.D.
+              </p>
+              <p className="text-base lg:text-xl">
+                Email: hirotani.jun.7v@kyoto-u.ac.jp
+              </p>
             </div>
             <div className="flex flex-col gap-2">
-              <p className="text-2xl">Associate Professor</p>
-              <p className="text-lg">
+              <p className="text-xl lg:text-2xl">Associate Professor</p>
+              <p className="text-base lg:text-lg">
                 Department of Micro Engineering, Graduate School of Engineering,
                 Kyoto University, JAPAN
               </p>
             </div>
-            <div className="self-start">
+            <div className="self-center lg:self-start">
               <Image
                 src={"/_images/KU_logo.png"}
                 width={200}
@@ -36,14 +42,14 @@ export default function Member() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-10 mt-8 lg:mt-0">
           <div className="flex flex-col gap-4">
             <div>
               <p className="text-2xl font-bold">EMPLOYMENT HISTORY</p>
             </div>
             <div>
-              <ul className="list-disc pl-4 text-lg">
-                <li className="text-xl">
+              <ul className="list-disc pl-4 text-base lg:text-lg">
+                <li>
                   PRESTO Researcher, Department of Strategic Basic Research,
                   Japan Science and Technology Agency, JAPAN (Dec. 2020 – Mar.
                   2024)
@@ -74,7 +80,7 @@ export default function Member() {
               <p className="text-2xl font-bold">EDUCATION</p>
             </div>
             <div>
-              <ul className="list-disc pl-4 text-lg">
+              <ul className="list-disc pl-4 text-base lg:text-lg">
                 <li>
                   Ph.D., Aeronautics and Astronautics, Kyushu University, JAPAN
                   (2013)
