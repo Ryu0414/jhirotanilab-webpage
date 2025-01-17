709986854a6977e9402585b30c80d08a8bb1372a
yamasakiryuto <y.ryuto20000414@gmail.com>
2025-01-17 11:21:30 +0900

    sitemap修正

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.jhirotanilab.com/",
+  siteUrl: "https://www.jhirotanilab.com/",
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: "./out",
 };
