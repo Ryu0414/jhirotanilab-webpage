2a473ceee7bd05f1f67de703227f49d10d995633
yamasakiryuto <y.ryuto20000414@gmail.com>
2025-02-14 11:29:38 +0900

    news id修正

--- src/app/_data/news.ts
+++ src/app/_data/news.ts
@@ -11,7 +11,7 @@ const NEWS_LIST: NewsType[] = [
     date: "February 6, 2025",
     description:
       "Our research paper was published in Journal of Applied Physics. The title of research was 'Estimating depth-directional thermal conductivity profiles using neural network with dropout in frequency-domain thermoreflectance' (Y. Ikeda et al.).",
-    id: 12,
+    id: 13,
     link: "https://doi.org/10.1063/5.0248430",
     imgPath: "/_images/news/20250206_ikeda_paper_cover.jpg",
   },
@@ -19,7 +19,7 @@ const NEWS_LIST: NewsType[] = [
     date: "January 9, 2025",
     description:
       "Our research paper was published in Review of Scientific Instruments. The title of research was 'Frequency-domain thermoreflectance with beam offset without the spot distortion for accurate thermal conductivity measurement of anisotropic materials' (Y. Akura et al.).",
-    id: 11,
+    id: 12,
     link: "https://doi.org/10.1063/5.0237004",
     imgPath: "/_images/news/20250109_akura_paper_cover.png",
   },
@@ -27,7 +27,7 @@ const NEWS_LIST: NewsType[] = [
     date: "January 2, 2025",
     description:
       "Our research paper was published in Applied Physics Letters. The title of research was 'Flexible 3ω sensors on submicron-thick parylene substrates for thermal conductivity measurements of liquids and soft materials' (R. Yamasaki et al.).",
-    id: 10,
+    id: 11,
     link: "https://doi.org/10.1063/5.0239596",
     imgPath: "/_images/news/20250102_yama_paper_image.png",
   },
@@ -35,7 +35,7 @@ const NEWS_LIST: NewsType[] = [
     date: "December 18, 2024",
     description:
       "Our research paper was published in AIP Advances. The title of research was 'Enhanced water adsorption properties of Ti3C2Tx MXene/bentonite thin films for highly sensitive humidity sensing' (H. Morotomi et al.).",
-    id: 9,
+    id: 10,
     link: "https://doi.org/10.1063/5.0245810",
     imgPath: "/_images/news/20241218_moromoro.jpg",
   },
@@ -43,7 +43,7 @@ const NEWS_LIST: NewsType[] = [
     date: "December 11, 2024",
     description:
       "We won the Excellence Award at Academia Award in Semicon Japan 2024!",
-    id: 8,
+    id: 9,
     link: "https://www.semiconjapan.org/jp/workforce/academia-award",
     imgPath: "/_images/news/20241211_semiconJapanAward.jpg",
   },
@@ -51,7 +51,7 @@ const NEWS_LIST: NewsType[] = [
     date: "December 10, 2024",
     description:
       "We will participate in SEMICON Japan at Tokyo International Exhibition Center from December 11 to 13.",
-    id: 7,
+    id: 8,
     link: "",
     imgPath: "/_images/news/20241210_semiconJapan.jpg",
   },
@@ -59,7 +59,7 @@ const NEWS_LIST: NewsType[] = [
     date: "December 6, 2024",
     description:
       "We presented at the MATERIALS RESEARCH SOCIETY (MRS). The titles of presentations were 'Carbon Nanotube Thin Film PN Junction Diode with High Temperature Tolerance Using Chemical Dopants.' (Y. Matsunaga et al.), 'Estimation of Thermal Conductivity Measurement of Ti3C2Tx MXene Thin Film Using Frequency-Domain Thermoreflectance' (Y. Akura et al.) and 'A Highly Sensitive Flexible Humidity Sensor Based on Ti3C2Tx MXene/Bentonite Thin Film' (H. Morotomi et al.).",
-    id: 6,
+    id: 7,
     link: "",
     imgPath: "/_images/news/20241206_MRS.jpg",
   },
@@ -67,7 +67,7 @@ const NEWS_LIST: NewsType[] = [
     date: "November 19, 2024",
     description:
       "We participated in a plant tour of JX Advanced Metals Corporation.",
-    id: 5,
+    id: 6,
     link: "",
     imgPath: "/_images/news/20241119_jx.JPG",
   },
@@ -75,7 +75,7 @@ const NEWS_LIST: NewsType[] = [
     date: "November 15, 2024",
     description:
       "We gave an invited talk on thermal management business at DEEPTECH KANSAI.",
-    id: 12,
+    id: 5,
     link: "https://osaka-startup.com/deeptech/",
     imgPath: "/_images/news/20241115_KANSAI.png",
   },
